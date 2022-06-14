723ec45153b1145b3480dd9b3cb74101719ceb79
MK <Morteza>
2022-06-14 15:23:02 +0300

    Initial Commit

---
+++ Movie SwiftUI/Movie_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_SwiftUIApp.swift
+//  Movie SwiftUI
+//
+//  Created by Morteza Kolivand on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Movie_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
